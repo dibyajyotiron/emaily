a3dbc74d883c77b003e5813944babacb8084d535
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-03-25 00:36:22 +0530

    no unused var exception for req,res,next,err

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
 	extends: 'airbnb-base',
 	rules: {
+		'no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: 'next|res|req|err',
+			},
+		],
 		'max-len': ['warn', { code: 80 }],
 		'global-require': 0,
 		'linebreak-style': 0,
