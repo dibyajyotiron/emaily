7ac90100c441f4c0d3be373cc4bd50bde1526157
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-03-25 00:40:28 +0530

    added routes for survey

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const authRoute = require('./authRoutes');
 const payment = require('./paymentRoutes');
-
-const auth = require('../middlewares/authMiddleware');
+const survey = require('./surveyRoutes');
 
 module.exports = (app) => {
   app.use(express.json());
   app.use('/auth', authRoute);
-  app.use('/api/payment', auth, payment);
+  app.use('/api/payment', payment);
+  app.use('/api/surveys', survey);
 };
---
+++ server/routes/surveyRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+
+const Survey = require('../models/survey');
+
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const auth = require('../middlewares/authMiddleware');
+const validateCredits = require('../middlewares/validateCredits');
+
+// eslint-disable-next-line no-unused-vars
+router.post('/', [auth, validateCredits], async (req, res) => {
+  const { title, subject } = req.body;
+  const { body, recipients } = req.body;
+
+  const survey = new Survey({
+    title,
+    subject,
+    body,
+    recipients: recipients.split(',').map(email => ({ email })),
+    _user: req.user.id,
+    dateSent: Date.now(),
+  });
+  try {
+    // send email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    await mailer.sendToSendGrid();
+    await survey.save();
+    req.user.credits -= 1;
+    await req.user.save();
+    res.json({ user: req.user });
+  } catch (error) {
+    res.status(422).json({
+      error: true,
+      message: error.message,
+    });
+  }
+});
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'Thanks for voting!',
+  });
+});
+module.exports = router;
