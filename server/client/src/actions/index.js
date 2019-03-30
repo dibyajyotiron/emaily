9d778cb14b9fa4aa21f927c7677dd153344e552a
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-03-31 04:36:29 +0530

    added action dispatch for submiting a survey

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -12,3 +12,16 @@ export const handleToken = token => async dispatch =>
 		type: FETCH_USER,
 		payload: (await axios.post("/api/payment", token)).data,
 	});
+
+export const submitSurvey = (values, history) => async dispatch => {
+	history.push("/surveys");
+	dispatch({
+		type: FETCH_USER,
+		payload: (await axios.post("/api/surveys", {
+			title: values.surveyTitle,
+			subject: values.surveySubject,
+			body: values.body,
+			recipients: values.recipients,
+		})).data,
+	});
+};
