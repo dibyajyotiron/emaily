ce94204a8a55f91117082644885c2ed23183ecf1
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-06 02:14:20 +0530

    added sorting logic

--- server/client/src/components/surveys/SurveyList/index.js
+++ server/client/src/components/surveys/SurveyList/index.js
@@ -27,24 +27,42 @@ class SurveyList extends Component {
 		const day = dateArr[0];
 		const [date, month, year, time, zone] = dateArr[1].trim().split(" ");
 		const processedTime = this.processTime(time);
-		const newDate = `${date}-${month}-${year}, ${day}, ${processedTime}`;
+		const newDate = `${day}, ${date}-${month}-${year}, ${processedTime}, ${zone}`;
 		return newDate;
 	}
 
 	renderSurveys() {
-		return this.props.surveys.reverse().map(survey => (
-			<div className="card darken-1" key={survey.id}>
-				<div className="card-content">
-					<span className="card-title">{survey.title}</span>
-					<p>{survey.body}</p>
-					<p className="right">Sent: {this.processDate(survey.dateSent)}</p>
+		return this.props.surveys
+			.map(survey => (
+				<div className="row">
+					<div className="container">
+						<div className="col s12 m12">
+							<div className="card teal lighten-1 hoverable" key={survey.id}>
+								<div className="card-content" style={{ color: "white" }}>
+									<span className="card-title"> {survey.title}</span>
+									<p>{survey.body}</p>
+									<p className="right">
+										Sent: <span>{this.processDate(survey.dateSent)}</span>
+									</p>
+								</div>
+								<div className="card-action teal lighten-5">
+									<a style={{ color: "black", textTransform: "none" }}>
+										Yes: <span style={{ color: "blue" }}>{survey.yes}</span>
+									</a>
+									<a style={{ color: "black", textTransform: "none" }}>
+										No: <span style={{ color: "red" }}>{survey.no}</span>
+									</a>
+									<a style={{ color: "black", textTransform: "none" }} className="right">
+										Last Responded:
+										<span style={{ color: "#107895" }}>{survey.lastResponded ? this.processDate(survey.lastResponded) : " Not yet responded!"}</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className="card-action">
-					<a> Yes: {survey.yes}</a>
-					<a> No: {survey.no}</a>
-				</div>
-			</div>
-		));
+			))
+			.reverse();
 	}
 
 	render() {
